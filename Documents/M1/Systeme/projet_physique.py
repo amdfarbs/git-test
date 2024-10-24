5ca48044e40c92cca4e55b263012466d5f7923ec
Ahmed <ahmed@macbook-air-de-ahmed.home>
2024-10-24 17:25:58 +0200

    First Commit

---
+++ Documents/M1/Systeme/projet_physique.py
@@ -0,0 +1,17 @@
+
+Longueur_onde=[]
+Intensité=[]
+fd=open("Spectre_photoluminescence.txt")
+for l in range (25):
+    next(fd)
+
+    
+for ligne in fd:
+    data=ligne.strip().split('\t').split(',')
+    print(data)
+    Longueur_onde.append(data[0])
+    Intensité.append(data[1])
+print(Longueur_onde)
+print(Intensité)
+print(data)
+fd.close()
